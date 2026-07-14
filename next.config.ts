cfadd99297a57e19e22d20ab72ca2805d0464765
buildwithjaym <jaymmaruji@gmail.com>
2026-07-14 21:53:10 +0800

    chore(next): update Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  images: {
+
+    remotePatterns: [
+
+      {
+        protocol: "https",
+        hostname: "cyytkwcglqpmbnrqtjdw.supabase.co",
+        pathname: "/storage/v1/object/public/**"
+      }
+
+    ]
+
+  }
+
 };
+
 
 export default nextConfig;
